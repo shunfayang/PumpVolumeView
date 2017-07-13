f63e922867097bca65e71ed7b03d49fe6c5838ad
yangshunfa <yang2time@163.com>
2017-07-13 16:27:54 +0800

    优化

--- app/src/main/java/com/yangshunfa/pumpvolumeview/PumpVolumeView.java
+++ app/src/main/java/com/yangshunfa/pumpvolumeview/PumpVolumeView.java
@@ -19,13 +19,15 @@ import android.view.View;
 
 public class PumpVolumeView extends View {
 
+    public static final String BRIGHT_GRAY = "#ff979797";
+    public static final String GREEN = "#ff21dc22";
     private Paint mPaint;
     private int mVolume = 100;
     private GestureDetector mDetector;
     private float centerWidth = 0;
     private float centerHeight = 0;
     private float top;
-    private Paint mPaintGray;
+//    private Paint mPaintGray;
     private float mProcessTop = 0;
     private float mDefaultProcess = 100;
     private float mX;
@@ -33,6 +35,7 @@ public class PumpVolumeView extends View {
     private float innerTop;
     private float innerBottom;
     private float mVolumeHeight;
+    private String darkGray = "#ff4a4a4a";
 
     public PumpVolumeView(Context context) {
         super(context);
@@ -49,48 +52,13 @@ public class PumpVolumeView extends View {
         init(context);
     }
     private void init(Context context) {
-        mPaintGray = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        mPaintGray = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintGray.setColor(Color.parseColor("#ff4a4a4a"));
         mPaint.setColor(Color.parseColor("#ff21dc22"));
-        mDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return false;
-            }
+        mPaint.setTextSize(20);
+        mPaint.setTextAlign(Paint.Align.CENTER);
 
-            @Override
-            public void onShowPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                float v = e2.getY() - e1.getY();
-                Log.d("moose", "" + v);
-                if (v > 50){
-                    // 递增
-                    mProcessTop ++;
-                    return true;
-                }
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                return false;
-            }
-        });
+        // 开启一个线程不断递减音量
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -114,16 +82,17 @@ public class PumpVolumeView extends View {
     protected void onDraw(Canvas canvas) {
         int measuredWidth = getMeasuredWidth();
         int measuredHeight = getMeasuredHeight();
+        // 最外边深色柱体
         float left = measuredWidth /2 - 20;
         float right = measuredWidth /2 + 20;
         top = measuredHeight / 2;
         float bottom = measuredHeight;
-        // 柱体
-        mPaintGray.setColor(Color.parseColor("#ff4a4a4a"));
-        canvas.drawRect(left, top, right, bottom, mPaintGray);
+        // 画柱体
+        mPaint.setColor(Color.parseColor(darkGray));
+        canvas.drawRect(left, top, right, bottom, mPaint);
 
         // inner rect
-        mPaintGray.setColor(Color.parseColor("#ff979797"));
+        mPaint.setColor(Color.parseColor(BRIGHT_GRAY));
         float innerLeft = left + 10;
         innerTop = top + 30;
         float innerRight = right - 10;
@@ -133,29 +102,30 @@ public class PumpVolumeView extends View {
         mVolumeHeight = (innerBottom - mProcessTop)/ volume;// 当前音量高度
         // 绘制音量数字
 
-//        TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        mPaintGray.setTextSize(20);
-        mPaintGray.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText((int)mVolumeHeight + "" , (right - left)/2 + left , top + 20,mPaintGray);
+        canvas.drawText((int)mVolumeHeight + "" , (right - left)/2 + left , top + 20,mPaint);
 
         // inner background
-        canvas.drawRect(innerLeft, innerTop, innerRight, innerBottom, mPaintGray);
-//        mPaint.setColor(Color.parseColor("#ff21dc22"));
-//        mProcessTop = innerTop + 50;
-        // 绘制默认手柄
+        canvas.drawRect(innerLeft, innerTop, innerRight, innerBottom, mPaint);
+        // 初始的进度
         if (centerHeight == 0 && centerWidth == 0 && mProcessTop == 0) {
-            mProcessTop = innerBottom - 10;
+            mProcessTop = innerBottom - 50;
             centerWidth = measuredWidth / 2;
             centerHeight = this.top - 50;
         }
+        // 绘制绿色的音量进度
+        mPaint.setColor(Color.parseColor(GREEN));
         canvas.drawRect(innerLeft, mProcessTop, innerRight, innerBottom, mPaint);
 
         float handleLeft = centerWidth - 50;
         float handleTop = centerHeight - 10;
         float handleRight = centerWidth + 50;
         float handleBottom = centerHeight;
-        canvas.drawRect(handleLeft, handleTop, handleRight, handleBottom, mPaintGray);
-        canvas.drawRect(centerWidth - 5, handleTop, centerWidth +5, this.top, mPaintGray);
+
+        mPaint.setColor(Color.parseColor(BRIGHT_GRAY));
+        // 横向手柄
+        canvas.drawRect(handleLeft, handleTop, handleRight, handleBottom, mPaint);
+        // 竖直打气杆
+        canvas.drawRect(centerWidth - 5, handleTop, centerWidth +5, this.top, mPaint);
 
     }
 
@@ -168,6 +138,7 @@ public class PumpVolumeView extends View {
                 float x = event.getX();
                 float y = event.getY();
                 if (y >= top ){
+                    // 手指移动距离低于外部柱体最高处
                     centerHeight = top;
                 } else {
                     if (y <= 20 ){
@@ -177,6 +148,7 @@ public class PumpVolumeView extends View {
                     }
                 }
                 if ((y - mY) > 0 && y <= top){
+                    // 手势往下“压”
                     if (mProcessTop >= innerTop) {
                         mProcessTop--;
                     }
